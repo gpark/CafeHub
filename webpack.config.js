dd08fc312724a6fddb0b86ebe92046e0141952e6
Justin Suen <jsuen27@gmail.com>
2017-03-17 12:07:23 -0700

    Basic frontend setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './frontend/cafehub.jsx',
+  output: {
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: [/\.jsx?$/, /\.js?$/],
+      exclude: /(node_modules)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }, {
+      test: /\.png$/,
+      loader: "url-loader?mimetype=image/png"
+    }]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       NODE_ENV: JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin()
+  // ]
+};
