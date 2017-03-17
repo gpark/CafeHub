8afc9a36adf4bc56e716edc0935a223473d47b65
Justin Suen <jsuen27@gmail.com>
2017-03-17 12:36:07 -0700

    Create api backend and move preference controller

---
+++ app/controllers/api/preference_controller.rb
@@ -0,0 +1,42 @@
+class Api::PreferenceController < ApplicationController
+  authorize_resource
+
+  def edit
+  end
+
+  def view
+    if current_user.preference.nil?
+      pref = Preference.create(minimum_hours: 8, maximum_hours: 20,
+                               daily_hours: 2, spread_out: false,
+                               time_of_week: 'Prefer weekdays',
+                               time_of_day: 'Prefer mornings')
+      current_user.preference = pref
+      current_user.save
+    end
+    @preference = current_user.preference
+    @entries = @preference.preference_entries
+  end
+
+  def change
+    @preference = current_user.preference
+    @preference.entries.destroy_all
+    preference_hash = params[:preference]
+    entries = preference_hash[:entries]
+    preference_hash.delete('entries')
+    @preference.assign_attributes(preference_hash)
+    entries.each do |entry|
+      entr = @preference.preference_entries.new
+      entr.name = entry.name
+      entr.start_time = entry.start_time.to_hr_form
+      entr.end_time = entry.end_time.to_hr_form
+      unless entr.save
+        redirect_to '/preference/edit', alert: 'Error submitting preference.'
+      end
+    end
+    if @preference.save
+      redirect_to '/preference/edit', notice: 'Preference submitted successfully.'
+    else
+      redirect_to '/preference/edit', alert: 'Error submitting preference.'
+    end
+  end
+end
---
+++ app/views/api/preference/view.json.jbuilder
@@ -0,0 +1,20 @@
+json.preference do
+
+  json.minimum_hours @preference.minimum_hours
+  json.maximum_hours @preference.maximum_hours
+  json.daily_hours @preference.daily_hours
+  json.spread_out @preference.spread_out
+  json.time_of_week @preference.time_of_week
+  json.time_of_day  @preference.time_of_day
+  json.obl1_expl @preference.obl1_expl
+  json.obl2_expl @preference.obl2_expl
+  json.obl3_expl @preference.obl3_expl
+  json.obl4_expl @preference.obl4_expl
+
+  json.entries @entries do |entry|
+    json.name entry.name
+    json.start_time entry.start_time.to_timestring
+    json.end_time entry.end_time.to_timestring
+  end
+
+end
