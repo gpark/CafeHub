cbf0a20e7d4a28e5db6e6f2cebf2bcba6949426f
Justin Suen <jsuen27@gmail.com>
2017-04-05 12:12:55 -0700

    Basic header

--- frontend/components/layout/header/header.jsx
+++ frontend/components/layout/header/header.jsx
@@ -4,17 +4,44 @@ import { Link, browserHistory, withRouter } from 'react-router';
 class Header extends Component {
   constructor(props) {
     super(props);
+
+    const { currentUser, logout } = this.props;
+  }
+
+  renderLinks() {
+    if (this.props.currentUser) {
+      return(
+        <ul className="nav navbar-nav navbar-right">
+          <li><Link to="/signup">Sign up</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      )
+    } else {
+      return(
+        <ul className="nav navbar-nav navbar-right">
+          <li><Link to="/signup">Sign up</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      )
+    }
   }
 
   render() {
-    const { currentUser, logout } = this.props;
-
     return (
-      <div className="app-header" ref="header">
-        <div className="logo">
-          <Link to="/"><h1>CafeHub</h1></Link>
+      <nav className="navbar navbar-default navbar-static-top" ref="header">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">
+              <h1>CafeHub</h1>
+            </Link>
+          </div>
         </div>
-      </div>
+        <div className="container-fluid">
+          <div id="navbar" className="navbar-collapse collapse">
+            { this.renderLinks() }
+          </div>
+        </div>
+      </nav>
     );
   }
 }
